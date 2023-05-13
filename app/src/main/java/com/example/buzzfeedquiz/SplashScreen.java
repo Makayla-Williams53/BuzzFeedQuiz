72cd1ec4040ca0ee39fc16b26dc25cdfdcc4bc83
Makayla <makaylawms0426@gmail.com>
2023-05-12 20:03:27 -0700

    created delay method then switched screens

--- app/src/main/java/com/example/buzzfeedquiz/SplashScreen.java
+++ app/src/main/java/com/example/buzzfeedquiz/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.buzzfeedquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        //creates handler post delay method in order to wait 5 seconds then change screens
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }//end run method
+        }, 5000);//end post delay
+    }//end onCreate
+}//end splashScreen
