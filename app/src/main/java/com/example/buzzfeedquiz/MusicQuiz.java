9e75751806b63b292eab079c6d8b819436c9d4ff
Makayla <makaylawms0426@gmail.com>
2023-05-15 12:18:11 -0700

    Have set up base components for the music quiz screen

--- app/src/main/java/com/example/buzzfeedquiz/MusicQuiz.java
+++ app/src/main/java/com/example/buzzfeedquiz/MusicQuiz.java
@@ -3,12 +3,18 @@ package com.example.buzzfeedquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
-public class MusicQuiz extends AppCompatActivity {
+public class MusicQuiz extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_quiz);
+
+        WebView webView = (WebView) findViewById(R.id.titleWeb);
+        webView.loadUrl("file:///android_asset/quiz.html");
     }
 }
