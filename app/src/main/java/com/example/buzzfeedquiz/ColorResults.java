e8ac7bc37bde4b089c5f2488bbb71be3e9c077b1
Makayla <makaylawms0426@gmail.com>
2023-05-14 21:57:20 -0700

    Have finished the results screen display

--- app/src/main/java/com/example/buzzfeedquiz/ColorResults.java
+++ app/src/main/java/com/example/buzzfeedquiz/ColorResults.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 public class ColorResults extends AppCompatActivity
 {
-
+    int color;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -16,5 +17,36 @@ public class ColorResults extends AppCompatActivity
 
         WebView webView = findViewById(R.id.titleWeb);
         webView.loadUrl("file:///android_asset/results.html");
-    }
-}
+
+        Bundle extras = getIntent().getExtras();
+        color = extras.getInt("results");
+
+        TextView resultView = findViewById(R.id.resultsView);
+
+        if(color == 1)
+        {
+            resultView.setText("Your Personality is: Red!!!");
+        }//end if
+        else if(color == 2)
+        {
+            resultView.setText("Your Personality is: Orange!!!");
+        }//end first else if
+        else if(color == 3)
+        {
+            resultView.setText("Your Personality is: Yellow!!!");
+        }//end second else if
+        else if(color == 4)
+        {
+            resultView.setText("Your Personality is: Green!!!");
+        }//end third else if
+        else if(color == 5)
+        {
+            resultView.setText("Your Personality is: Blue!!!");
+        }//end fourth else if
+        else if(color == 6)
+        {
+            resultView.setText("Your Personality is: Purple!!!");
+        }//end fifth else if
+    }//end onCreate
+
+}//end colorResults
