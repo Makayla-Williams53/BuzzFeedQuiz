5fc996b33c7d001e3402fc9b07b3a772802d7396
Makayla <makaylawms0426@gmail.com>
2023-05-14 21:43:37 -0700

    Have added base components for the results screen

--- app/src/main/java/com/example/buzzfeedquiz/ColorResults.java
+++ app/src/main/java/com/example/buzzfeedquiz/ColorResults.java
@@ -3,12 +3,18 @@ package com.example.buzzfeedquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
-public class ColorResults extends AppCompatActivity {
+public class ColorResults extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_results);
+
+        WebView webView = findViewById(R.id.titleWeb);
+        webView.loadUrl("file:///android_asset/results.html");
     }
 }
