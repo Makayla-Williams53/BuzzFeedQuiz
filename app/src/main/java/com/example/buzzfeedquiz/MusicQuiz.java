46438f3f1273f12555e6a8cf17bcb2fcddfe56a3
Makayla <makaylawms0426@gmail.com>
2023-05-21 12:46:54 -0700

    Have finished question 2

--- app/src/main/java/com/example/buzzfeedquiz/MusicQuiz.java
+++ app/src/main/java/com/example/buzzfeedquiz/MusicQuiz.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
