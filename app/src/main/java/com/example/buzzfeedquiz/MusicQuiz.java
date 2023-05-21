5cb77fcd4d8feb042c0057fdfe71043f4ba161a8
Makayla <makaylawms0426@gmail.com>
2023-05-21 13:18:43 -0700

    Have finished the third question and the necessary methods

--- app/src/main/java/com/example/buzzfeedquiz/MusicQuiz.java
+++ app/src/main/java/com/example/buzzfeedquiz/MusicQuiz.java
@@ -18,6 +18,7 @@ public class MusicQuiz extends AppCompatActivity
 
     int answer1 = 0;
     int answer2 = 0;
+    int answer3 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -101,10 +102,55 @@ public class MusicQuiz extends AppCompatActivity
         }//end second else if
     }//end select2
 
+    public void select3(View view)
+    {
+        Button button1 = findViewById(R.id.q3opt1);
+        Button button2 = findViewById(R.id.q3opt2);
+        Button button3 = findViewById(R.id.q3opt3);
+        Button button4 = findViewById(R.id.q3opt4);
+        Button button5 = findViewById(R.id.q3opt5);
+        Button button6 = findViewById(R.id.q3opt6);
+
+        button1.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button2.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button3.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button4.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button5.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button6.setBackgroundColor(Color.parseColor("#76E5FC"));
+
+        int id = view.getId();
+        Button button = findViewById(id);
+        button.setBackgroundColor(Color.parseColor("#4BC0D9"));
+        if(button == button1)
+        {
+            answer3 = 1;
+        }//end if
+        else if(button == button2)
+        {
+            answer3 = 2;
+        }//end first else if
+        else if(button == button3)
+        {
+            answer3 = 3;
+        }//end second else if
+        else if(button == button4)
+        {
+            answer3 = 4;
+        }//end third else if
+        else if(button == button5)
+        {
+            answer3 = 5;
+        }//end fourth else if
+        else if(button == button6)
+        {
+            answer3 = 6;
+        }//end last else if
+    }//end select3
+
     public void analyze(View view)
     {
         Queue<Integer> answers = new LinkedList<Integer>();
-        if(answer1 == 0 || answer2 == 0)
+        if(answer1 == 0 || answer2 == 0 || answer3 == 0)
         {
             Toast.makeText(this, "Please answer all the questions!", Toast.LENGTH_SHORT).show();
         }//end if
@@ -112,6 +158,12 @@ public class MusicQuiz extends AppCompatActivity
         {
             answers.add(answer1);
             answers.add(answer2);
+            int temp3 = ((answer3 - 5) + 6) % 6;
+            if(temp3 == 0)
+            {
+                temp3 = 6;
+            }//end if
+            answers.add(temp3);
         }//end else
     }//end analyze
 }//end MusicQuiz
