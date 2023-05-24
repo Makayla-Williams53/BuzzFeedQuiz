d24beed0165795a08ca55628bfc0e1042a03df95
Makayla <makaylawms0426@gmail.com>
2023-05-23 17:48:55 -0700

    Have finished the fifth question as a whole

--- app/src/main/java/com/example/buzzfeedquiz/MovieQuiz.java
+++ app/src/main/java/com/example/buzzfeedquiz/MovieQuiz.java
@@ -19,6 +19,7 @@ public class MovieQuiz extends AppCompatActivity {
     int answer2 = 0;
     int answer3 = 0;
     int answer4 = 0;
+    int answer5 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -206,12 +207,57 @@ public class MovieQuiz extends AppCompatActivity {
         {
             answer4 = 6;
         }//end last else if
-    }//end select3
+    }//end select4
+
+    public void select5(View view)
+    {
+        Button button1 = findViewById(R.id.q5opt1);
+        Button button2 = findViewById(R.id.q5opt2);
+        Button button3 = findViewById(R.id.q5opt3);
+        Button button4 = findViewById(R.id.q5opt4);
+        Button button5 = findViewById(R.id.q5opt5);
+        Button button6 = findViewById(R.id.q5opt6);
+
+        button1.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button2.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button3.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button4.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button5.setBackgroundColor(Color.parseColor("#76E5FC"));
+        button6.setBackgroundColor(Color.parseColor("#76E5FC"));
+
+        int id = view.getId();
+        Button button = findViewById(id);
+        button.setBackgroundColor(Color.parseColor("#4BC0D9"));
+        if(button == button1)
+        {
+            answer5 = 1;
+        }//end if
+        else if(button == button2)
+        {
+            answer5 = 2;
+        }//end first else if
+        else if(button == button3)
+        {
+            answer5 = 3;
+        }//end second else if
+        else if(button == button4)
+        {
+            answer5 = 4;
+        }//end third else if
+        else if(button == button5)
+        {
+            answer5 = 5;
+        }//end fourth else if
+        else if(button == button6)
+        {
+            answer5 = 6;
+        }//end last else if
+    }//end select4
 
     public void analyze(View view)
     {
         Queue<Integer> answers = new LinkedList<>();
-        if(answer1 == 0 || answer2 == 0 || answer3 == 0 || answer4 == 0)
+        if(answer1 == 0 || answer2 == 0 || answer3 == 0 || answer4 == 0 || answer5 == 0)
         {
             Toast.makeText(this, "Please answer all the questions!", Toast.LENGTH_SHORT).show();
         }//end if
@@ -236,7 +282,13 @@ public class MovieQuiz extends AppCompatActivity {
                 temp4 = 6;
             }//end if
             answers.add(temp4);
-            Log.i("info", "Answer4: " + temp4);
+            int temp5 =((answer5 - 2) + 6) % 6;
+            if(temp5 == 0)
+            {
+                temp5 = 6;
+            }//end if
+            answers.add(temp5);
+            Log.i("info", "Answer5: " + temp5);
         }//end else
     }//end analyze
 
